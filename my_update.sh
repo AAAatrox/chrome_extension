0c849b5a5551c3b940f6011d834931e04051d4da
Lynx <1197225628@qq.com>
2019-04-21 09:11:14 +0800

    CORS

--- my_update.sh
+++ my_update.sh
@@ -1,8 +1,13 @@
-if [ "$1"x = x ]
+if [ $1"x" = x ]
 then
-	echo "need message"
+  echo "need message"
 else
-	git add .
-	git commit -m $1
-	git push origin master
+  git add .
+  git commit -m $1
+  if [ $2"x" = x ]
+  then
+    echo "no pushing"
+  else
+    git push origin master
+  fi
 fi
